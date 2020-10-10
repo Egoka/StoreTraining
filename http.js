ef57ebc8f7273d430003e957b316b9b83819ae3d
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-10 23:25:43 +0300

    Site output
    In the case of working via the GET protocol, the response object will write the parameters of the Head area in the writeHead method. Then the write method will receive and output data. Everything will end with the end method to mark the end of the query execution.

--- http.js
+++ http.js
@@ -1,6 +1,15 @@
 const http = require('http')
 const server = http.createServer((request,response)=>{
     if (request.method==='GET'){
+        response.writeHead(200, {'Content-Type': `text/html`})
+        response.write(`
+        <h1>Form</h1>
+        <form method="post" action="/">
+            <input name="title" type="text"/>
+            <button type="submit">Send</button>
+        </form>
+        `)
+        response.end()
     }
     else if (request.method === 'POST'){
     }
