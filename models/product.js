ece523b6f34354068187d53870fd56e180faf482
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-15 17:45:12 +0300

    Database instance creation
    Two entities are imported from the 'mongoose' package: Schema to create a new database object and model to configure the imported object.
    The created product object from the Schema () class creates three fields: title, price and img.
    Each object has assigned properties: type, required.

---
+++ models/product.js
@@ -0,0 +1,16 @@
+const {Schema,model} = require('mongoose')
+
+const product = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    img:{ type: String }
+
+})
+
+module.exports = model('Product', product)
