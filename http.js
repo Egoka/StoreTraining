bf638d8d101863711628a1883dc227954fa9d0b7
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-15 16:38:37 +0300

    Launching an application with a connected database
    Included the 'mongoose' library and the URL variable from the password.js file.
    The launch of the application has been moved to the asynchronous start () function, where the entire launch is placed in a try catch (err) construction.
    The connection to the database is made using the connect () method of the mongoose object, where the first argument is the URL to the database.
    Next, the application is launched.

--- http.js
+++ http.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const URL = require('./password')
 const app = express()
 
 const PORT = process.env.PORT || 3000
-
-app.listen(3000,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+async function start(){
+    try {
+        await mongoose.connect(URL, {useNewUrlParser: true,useUnifiedTopology: true})
+        app.listen(3000, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    }catch(err){
+        console.log(err)}
+}
+start()
