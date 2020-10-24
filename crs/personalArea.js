0ab0db29aa9a3209c15daeb1c0375c83abd6b54a
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-24 15:08:56 +0300

    Personal account page
    + The content of the personalArea.js file has been moved to the new productAddition.js file. All personalArea variables have been renamed to productAddition.
    + A personal account page has been created with variable parameters, name and photo, which can be downloaded.
    + For the "personal account" page, a router file with a get() method was created to load the page with parameters.

--- crs/personalArea.js
+++ crs/personalArea.js
@@ -1,39 +1,12 @@
 const {Router} = require('express')
-const {validationResult} = require('express-validator')
-const Product = require('../models/product')
 const closedPage = require('../middleware/pageAccess')
-const {productsValidators} = require('../utils/validators')
 const router = Router()
-router.get('/', closedPage,(req, res) => {
+router.get('/', closedPage, async(req,res)=>{
     res.render('personalArea',{
-        title: 'Личный кабинет',
-        isArea: true
+        title:'Профиль',
+        isArea: true,
+        user: req.user.toObject()
     })
 })
-router.post('/', closedPage,productsValidators,async (req, res) => {
-    const error = validationResult(req)
-    if(!error.isEmpty())
-        return res.status(422).render('personalArea',{
-            title: 'Личный кабинет',
-            isArea: true,
-            error: error.array()[0].msg,
-            date:{
-                title:req.body.title,
-                price:req.body.price,
-                img: req.body.img
-            }
-        })
-    const product = new Product({
-        title:req.body.title,
-        price:req.body.price,
-        img: req.body.img,
-        userId: req.user
-    })
-    try {
-        await product.save()
-        res.redirect('/products')
-    }catch(arr){
-        console.log(err)
-    }
-})
+router.post('/', async(req,res)=>{})
 module.exports = router
